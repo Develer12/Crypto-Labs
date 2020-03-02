7f0ea0423fee5ea163e4004600a487597b0361e3
Develer12 <cenia-v@mail.ru>
2020-03-02 14:37:04 +0300

    Completed

--- Lab5/index.js
+++ Lab5/index.js
@@ -15,22 +15,22 @@ const Alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
 
 console.log(`Aphabet: ${Alphavit}`);
 
-function ZigWay(){
+function ZigWay(Alphavi){
     let Alpha = [];
     let n = 0;
-    let arrCount = Alphavit.length;
-    let count = Alphavit[0].length;
+    let arrCount = Alphavi.length;
+    let count = Alphavi[0].length;
     for(let i = 0; i <= count; i++){
         if(i%2 == 1){
             for(let j = 0; j < arrCount; j++){
-                Alpha[n++] = Alphavit[j][i];
+                Alpha[n++] = Alphavi[j][i];
                 if(n == 27)
                 break;
             }
         }
         else{
             for(let j = arrCount-1; j >= 0; j--){
-                Alpha[n++] = Alphavit[j][i];
+                Alpha[n++] = Alphavi[j][i];
                 if(n == 27)
                 break;
             }
@@ -45,7 +45,7 @@ function ZigWay(){
 function WaySH(key){
     console.log('WaySH');
     let timer = Date.now();
-    let Alpha = ZigWay();
+    let Alpha = ZigWay(Alphavit);
     key = key.toLowerCase();
     let keySH = [];
 
@@ -80,19 +80,66 @@ function WaySH(key){
     console.log('--------------------------------------------');
 }
 
-function ManySH(key){
+function ManySH(key1, key2){
     console.log('ManySH');
     let timer = Date.now();
-    key = key.toLowerCase();
+    key1 = key1.toLowerCase();
+    key2 = key2.toLowerCase();
     let keySH = [];
 
-    let Alpha = [];
-
-    for(let i = 0; i < key.length ; i++){
-        keySH[i] = Alpha[Alphabet.indexOf(key[i])];
+    let Alpha = new Array(key1.length);
+    for(let i = 0; i < Alpha.length; i++){
+        Alpha[i] = new Array(key2.length);
+    }
+    let AlphaSH = new Array(key1.length);
+    for(let i = 0; i < AlphaSH.length; i++){
+        AlphaSH[i] = new Array(key2.length);
+    }
+    let n = 0;
+    for(let i = 0; i < key1.length; i++){
+        for(let j = 0; j < key2.length; j++){
+            if(n < Alphabet.length)
+                Alpha[i][j] = Alphabet[n++];
+            else
+                Alpha[i][j] = ' ';
+        }
     }
 
-    console.log('Key: ' + key);
+    let Akey1 = [];
+    for(k in key1){
+        Akey1[k] = key1[k];
+    }
+    Akey1.sort();
+    for(k in key1){
+        for(l in Akey1){
+            if(Akey1[l] == key1[k])
+                AlphaSH[k] = Alpha[l];
+        }
+    }
+
+    let Akey2 = [];
+    for(k in key2){
+        Akey2[k] = key2[k];
+    }
+
+    Akey2.sort();
+
+    for(k in key2){
+        for(l in Akey2){
+            if(Akey2[l] == key2[k])
+                for(let i = 0; i < AlphaSH.length; i++){
+                    AlphaSH[i][k] = Alpha[i][l];
+                }
+        }
+    }
+    for(l in AlphaSH){
+        console.log('alphash '+l+': '+AlphaSH[l]);
+    }
+
+    keySH = ZigWay(AlphaSH);
+
+
+
     console.log('KeySH: ' + keySH.join(''));
     console.log(`Time: ${Date.now() - timer}`)
 
